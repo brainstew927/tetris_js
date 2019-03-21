f99fb59ee4865c8f933ba4c3fc446104d2d4514a
lorenzo <brainstew927@gmail.com>
2019-03-21 17:19:54 +0100

    prove per array!

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 var x = 0;
 var y = 0;
 
+var mappa = [
+  [1,1,1],
+  [0,0,0],
+  [0,0,0]
+];
+
 function su(variabile){
 
       return variabile - 1;
@@ -17,7 +23,7 @@ function des(variabile){
 function sin(variabile){
   return variabile -1;
 }
-
+/*
 
 for(var i = 0; i < 10; i++){
   x = giu(x);
@@ -29,3 +35,10 @@ for(var i = 0; i < 10; i++){
   x = su(x);
   console.log(x);
 }
+*/
+for (var i = 0; i < mappa[0].length; i++) {
+    console.log(mappa[0][i]);
+    console.log("sep ");
+}
+
+//console.log(mappa[0].length);
