0407ab690d8a93cb0c829cc3ac03981f337aab1b
lorenzo <brainstew927@gmail.com>
2019-03-21 17:10:46 +0100

    aggiunte funzioni per movimento blocco

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-Window.
+
+var x = 0;
+var y = 0;
+
+function su(variabile){
+
+      return variabile - 1;
+
+}
+function giu(variabile) {
+  return variabile + 1;
+}
+
+function des(variabile){
+  return variabile +1;
+}
+function sin(variabile){
+  return variabile -1;
+}
+
+
+for(var i = 0; i < 10; i++){
+  x = giu(x);
+  console.log(x);
+}
+console.log("_________________________-");
+
+for(var i = 0; i < 10; i++){
+  x = su(x);
+  console.log(x);
+}
